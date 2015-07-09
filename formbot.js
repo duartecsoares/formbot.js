9d28a47ef536c5da8f8f37fff71be4def5880949
Duarte Soares <duartecsoares@me.com>
2015-07-09 04:19:21 +0100

    messages added

--- formbot.js
+++ formbot.js
@@ -14,6 +14,9 @@
  *
  */
 (function (root, factory) {
+
+    "use strict";
+
     if (typeof define === 'function' && define.amd) {
         define([], factory);
     } else if (typeof exports === 'object') {
@@ -21,14 +24,36 @@
     } else {
         root.Formbot = factory;
     }
-}(this, function Formbot(){
+}(this, function Formbot(settings){
 
-    function _constructor(){
+    //private interface
+    var messages = {
 
-        console.log("init");
+        "min"       : "Field must have atleast %p1 characters",
+        "max"       : "Field cant have more than %p1 characters",
+        "required"  : "Field is required.",
+        "default"   : "Field is invalid"
 
-    }
+        },
 
-    _constructor();
+        /**
+         * Constructor method to set settings
+         *
+         * @private
+         * @param {Object} settings Config object
+         */
+        _constructor = function(settings){
+
+            console.log("init", settings);
+
+            return settings;
+
+        };
+
+
+    //public interface
+    this.add = function(){} //add validations
+
+    _constructor(settings);
 
 }));
